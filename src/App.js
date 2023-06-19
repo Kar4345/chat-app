344637699d022043846877911b90f7ce0d11b182
Kar4345 <kartik21gr@gmail.com>
2023-06-19 17:19:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "../src/css/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {/* <h1>This is start page of this project!!!</h1> */}
+        <h1>Welcome to MyChat App!</h1>
+      </div>
+      <div id="intButton">
+        <button>Get Started</button>
+        <button>Log in</button>
+      </div>
+    </>
   );
 }
 
